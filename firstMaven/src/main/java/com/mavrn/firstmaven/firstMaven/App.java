ed946b10c2ff7be3b7e3f419f01e2bce8e5abb43
onlywyj <wyj669666@outlook.com>
2020-02-24 14:44:31 +0800

    简单Maven项目开发示例

---
+++ firstMaven/src/main/java/com/mavrn/firstmaven/firstMaven/App.java
@@ -0,0 +1,13 @@
+package com.mavrn.firstmaven.firstMaven;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}

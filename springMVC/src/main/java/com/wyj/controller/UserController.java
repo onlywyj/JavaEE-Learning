6c70e80f09312e77f26a6371960cda5e0c6d117e
onlywyj <wyj669666@outlook.com>
2020-04-27 10:40:43 +0800

    springMVC核心类和注解

--- springMVC/src/main/java/com/wyj/controller/UserController.java
+++ springMVC/src/main/java/com/wyj/controller/UserController.java
@@ -1,31 +1,37 @@
 package com.wyj.controller;
 
 import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
 
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.servlet.ModelAndView;
 
 @Controller
 public class UserController {
 	
 	
-	@RequestMapping("/login")
+	@RequestMapping(value="/login",name="用户登录",method=RequestMethod.GET)
 	public String login() {
 		System.out.println("login...");
-		return "forward:/WEB-INF/jsp/login.jsp";
+		return "login";
 	}
 
 	@RequestMapping("/checkUser")
-	public ModelAndView checkUser(HttpServletRequest request) {
+	public String checkUser(HttpServletRequest request,HttpServletResponse response,Model model) {
 		String username = request.getParameter("username");
 		String password = request.getParameter("password");
 		System.out.println(username+"  "+password);
-		ModelAndView mav = new ModelAndView();
-		mav.addObject("username",username);
-		mav.addObject("password",password);
-		mav.setViewName("forward:/WEB-INF/jsp/hello.jsp");
-		return mav;
+		//ModelAndView mav = new ModelAndView();
+		//mav.addObject("username",username);
+		//mav.addObject("password",password);
+		//mav.setViewName("forward:/WEB-INF/jsp/hello.jsp");
+		model.addAttribute("username", username);
+		model.addAttribute("password", password);
+		//return mav;
+		return "hello";
 		
 	}
 }
